f0783ba00ff97f605fc920ec1aa9ee6e778cf22c
Курчаков Даниил <mintor18@mail.ru>
2025-09-22 14:46:28 +0300

    2ая лаба 2 задание pair

--- 3zad.cpp
+++ 3zad.cpp
@@ -15,7 +15,7 @@ string generateSecretNumber(){
     secret += to_string(firstDigit);
     used[firstDigit] = true;
 
-    for (int = 1; i < 4; i++) {
+    for (int i = 1; i < 4; i++) {
         int digit;
         do {
             digit = rand() % 10;
@@ -23,5 +23,24 @@ string generateSecretNumber(){
         secret += to_string(digit);
         used[digit] = true;
     }
-        return secret
+        return secret;
+}
+pair<int, int> countBAC(const string& secret, const string& guess){
+    int bulls = 0;
+    int cows = 0;
+
+    for (int i = 0; i < 4; i++) {
+        if (guess[i] == secret[i]) {
+            bulls++;
+        } else {
+            for (int j = 0; j < 4; j++)
+                {
+                    if (i != j && guess[i] == secret[j]) {
+                        cows++;
+                        break;
+                    }
+                }
+            }
+    }
+    return make_pair(bulls, cows);
 }
