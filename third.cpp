16b740e709397d51e4c979ab6442f0c6a1958bb6
Курчаков Даниил <mintor18@mail.ru>
2025-09-22 13:48:01 +0300

    йоу

---
+++ third.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+void createFile(const std::string& filename) {
+    std::ofstream file(filename);
+    if (!file) {
+        std::cerr << "cant open file " << filename << std::endl;
+        return;
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        int randomNumber = rand() % 10 + 1;
+        file << randomNumber;
+        if (i < 9) {
+            file << " ";
+        }
+    }
+    file.close();
+}
+
+int main() {
+    srand(static_cast<unsigned int>(time(0)));
+    createFile("file1.txt");
+    createFile("file2.txt");
+    createFile("file3.txt");
+
+    std::cout << "file created." << std::endl;
+    return 0;
+}
